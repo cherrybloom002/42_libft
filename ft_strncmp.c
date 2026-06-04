97a96616f0d81a36cf28648517ef629aaf5ad4db
Andreea Manuela Hodor <ahodor@c2r1s7.42roma.it>
2026-06-04 11:10:33 +0200

    fix(ft): fixed ft_strncmp behaviour

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,22 +6,21 @@
 /*   By: ahodor <ahodor@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 10:33:01 by ahodor            #+#    #+#             */
-/*   Updated: 2026/06/03 16:52:38 by ahodor           ###   ########.fr       */
+/*   Updated: 2026/06/04 11:09:24 by ahodor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t			i;
-	unsigned char	*str1;
-	unsigned char	*str2;
-	
+	size_t i;
+	unsigned char *str1;
+	unsigned char *str2;
+
 	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	
 	if (n == 0)
 		return (0);
 	while (str1[i] && str2[i] && str1[i] == str2[i] && i < n - 1)
